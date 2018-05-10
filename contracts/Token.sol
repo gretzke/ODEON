3b5af0167a11c6d88fd1f98ff076e68aacafe841
Daniel Gretzke <daniel@gretzke.de>
2018-05-10 19:02:53 +0200

    Basic ERC20 Token functionality

---
+++ contracts/Token.sol
@@ -0,0 +1,106 @@
+pragma solidity 0.4.23;
+
+// Safe Math library that automatically checks for overflows and underflows
+library SafeMath {
+    // Safe multiplication
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        require(a == 0 || c / a == b);
+        return c;
+    }
+    // Safe subtraction
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a);
+        return a - b;
+    }
+    // Safe addition
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c>=a && c>=b);
+        return c;
+    }
+}
+
+/// @title Owned contract
+/// @author InnoChain
+contract Owned {
+    address public owner;
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "msg.sender isn't owner");
+        _;
+    }
+    function Owned() public {
+        owner = msg.sender;
+    }
+}
+
+/// @title ERC20 Token contract
+/// @author InnoChain
+/// @notice Basic ERC20 functionality
+contract Token is Owned {
+    using SafeMath for uint256;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    // This creates a mapping with all balances
+    mapping (address => uint256) public balanceOf;
+    // Another mapping with spending allowances
+    mapping (address => mapping (address => uint256)) public allowance;
+    // The total supply of the token
+    uint256 public totalSupply;
+    // variables for wallet integration
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+
+    function Token() public {
+        name = "Odeon Mining Token";
+        symbol = "OMT";
+        decimals = 18;
+    }
+
+    // Send coins
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != 0x0);
+        require(_to != address(this));
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != 0x0);
+        require(_to != address(this));
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+    
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool success) {
+        allowance[msg.sender][_spender] = allowance[msg.sender][_spender].add(_addedValue);
+        emit Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
+        return true;
+    }
+    
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool success) {
+        uint256 oldValue = allowance[msg.sender][_spender];
+        if (_subtractedValue > oldValue) {
+            allowance[msg.sender][_spender] = 0;
+        } else {
+            allowance[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+        }
+        emit Approval(msg.sender, _spender, allowance[msg.sender][_spender]);
+        return true;
+    }
+}
